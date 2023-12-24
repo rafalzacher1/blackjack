1e56b89b6de007109144313360c387ebdb5ba6bf
rafalzacher1 <rafalzacher99@gmail.com>
2023-12-24 13:16:40 +0000

    Added a public dir to store all client pages/scripts and adjusted the src links.

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Serve static files from the 'engine' directory
+app.use('/scripts', express.static('scripts'));
+
+// Serve static files from the 'cards' directory
+app.use('/cards', express.static('cards'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'interface.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
